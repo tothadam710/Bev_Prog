549c905a04f415bece309c8a3943cc8543594a16
Adam Toth <46538598+tothadam710@users.noreply.github.com>
2021-09-21 22:12:02 +0200

    Add files via upload

---
+++ CH5/error.cpp
@@ -0,0 +1,59 @@
+#include "std_lib_facilities.h"
+
+
+int main()
+
+try {
+
+//cout << "Success!\n";
+//cout << "Success!\n";
+//cout << "Success" << "!\n";
+//cout << "Success" << "!\n";
+//int res = 7; vector<int> v(10); v[5] = res; cout << "Success!\n";
+//vector<int> v(10); v[5] = 7; if(v[5]==7) cout << "Success!\n";
+//int cond = 1;if(cond){ cout << "Success!\n";} else{ cout << "Fail!\n";}
+//bool c = false; if(!c) cout << "Success!\n"; else cout << "Fail!\n";   // or bool c = true; if(c) cout << "Success!\n"; else cout << "Fail!\n";
+//string s = "ape"; bool c = "fool"<s; if (!c) cout << "Success!\n";  // or string s = "ape"; bool c = "fool">s; if (c) cout << "Success!\n"; 
+//string s = "ape"; if (s!="fool") cout << "Success!\n";
+//string s = "ape"; if (s!="fool") cout << "Success!\n";
+//string s = "ape"; if(s+"fool"=="apefool") cout << "Success!\n";
+//vector<char> v(1); for (int i=0; i<v.size();++i) cout << "Success!\n";
+//vector<char> v(5); for (int i=5; i<=v.size();++i) cout << "Success!\n";
+//string s = "Success!\n"; for (int i=0; i<9; ++i) cout << s[i];
+//if (true) then cout << "Success!\n"; else cout << "Fail!\n";
+//int x = 2000; int c = x; if (c==2000) cout << "Success!\n";
+//string s = "Success!\n"; for (int i=0; i<10; ++i) cout << s[i];
+//vector<int> v(1); for (int i=1; i<=v.size();++i) cout << "Success!\n";
+//int i=0; int j = 9; while (i<10) ++i; if (j<i) cout << "Success!\n";
+//double x = 2; double d = 5/(x / 2); if (d==2*(x+0.5)) cout << " Success!\n " ;
+//string s = "Success!\n"; for (int i=0; i<=10; ++i) cout << s[i];
+//int i=0; int j = 0; while (i<10) ++i; if (j<i) cout << "Success!\n";
+//double x = 3.11111111; double d = 5/(x - 2); if (d=2*x+0.5) cout << "Success!\n";
+//cout << "Success!\n";
+
+
+
+
+
+
+
+	keep_window_open();
+return 0;
+
+
+}
+
+
+
+catch (exception& e) {
+
+cerr << "error: " << e.what() << '\n';
+
+keep_window_open();
+
+return 1;
+
+}
+
+
+
