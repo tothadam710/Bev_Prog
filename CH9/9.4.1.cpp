1bf52380fa5c7cedd97cec82dc321619a6b5c069
Adam Toth <46538598+tothadam710@users.noreply.github.com>
2021-10-29 23:52:00 +0200

    Add files via upload

---
+++ CH9/9.4.1.cpp
@@ -0,0 +1,95 @@
+#include "std_lib_facilities.h"
+
+
+
+struct Date {
+
+    int ev;
+    int honap;
+    int nap;
+
+
+   
+
+};
+
+void init_day(Date& dd,int e,int h,int n) {
+
+    if (e <= 0) {
+        error("Hibas ev!");
+    }
+
+    else {
+        dd.ev = e;
+    }
+    if (h > 12 || h <= 0) {
+        error("Hibas honap!");
+    }
+
+    else {
+        dd.honap = h;
+    }
+
+    if (n > 0 && n < 31) {
+        if (h==2 && n > 28) {
+            error("A februar 28 napos!");
+        }
+
+        else {
+            dd.nap = n;
+        }
+    }
+
+    else {
+        error("Hibas honap!");
+    }
+
+
+}
+
+void add_day(Date& dd, int hozzaad) {
+    
+    dd.nap = dd.nap + hozzaad;
+
+    if (dd.nap > 31) {
+        dd.honap++;
+        dd.nap = dd.nap - 31;
+        if (dd.honap > 12) {
+            dd.ev++;
+            dd.honap = dd.honap - 12;
+        }
+    }
+
+
+}
+
+
+int main()
+{
+    try {
+        
+
+       
+
+      
+        Date tomorrow;
+        init_day(tomorrow, 1978, 6, 25);
+
+        cout << "today:" << tomorrow.ev << '.' << tomorrow.honap << '.' << tomorrow.nap << ".\n";
+
+        
+        
+        cout << "tomorrow:";
+        add_day(tomorrow,1);
+
+       
+        cout << tomorrow.ev << '.' << tomorrow.honap << '.' << tomorrow.nap << ".\n";
+
+        return 0;
+    }
+    catch (exception& e) {
+        cout << e.what() << endl;
+        return 1;
+    }
+}
+
---
+++ CH9/9.4.3.cpp
@@ -0,0 +1,117 @@
+#include "std_lib_facilities.h"
+
+class Date {
+private:
+	int ev;
+	int ho;
+	int nap;
+
+public:
+    Date(int e, int h, int n);
+    void add_day(int hozzaad);
+
+    int get_ev() {
+        return ev;
+    }
+
+    int get_ho() {
+        return ho;
+    }
+
+    int get_nap(){
+        return nap;
+    }
+
+    void kiiras() {
+        cout << get_ev() << '.' << get_ho() << '.' << get_nap() << '.' << endl;
+    }
+
+    
+    
+
+};
+
+
+Date::Date(int e, int h, int n) {
+
+    if (e <= 0) {
+        error("Hibas ev!");
+    }
+
+    else {
+        ev = e;
+    }
+    if (h > 12 || h <= 0) {
+        error("Hibas honap!");
+    }
+
+    else {
+        ho = h;
+    }
+
+    if (n > 0 && n < 31) {
+        if (h == 2 && n > 28) {
+            error("A februar 28 napos!");
+        }
+
+        else {
+            nap = n;
+        }
+    }
+
+    else {
+        error("Hibas honap!");
+    }
+
+
+}
+
+void Date::add_day(int hozzaad) {
+    nap = nap + hozzaad;
+
+    if (nap > 31) {
+        ho++;
+        nap = nap - 31;
+      
+        if (ho > 12) {
+            ev++;
+            ho = ho - 12;
+                    
+        }
+
+      
+        
+        
+
+    }
+
+}
+
+
+
+int main() {
+
+
+    try {
+
+        Date today( 1978, 6, 25 );
+
+        cout << "today: ";
+        today.kiiras();
+
+        today.add_day(1);
+
+        cout << "tomorrow: ";
+        today.kiiras();
+
+
+        return 0;
+    }
+    catch (exception& e) {
+        cout << e.what() << endl;
+        return 1;
+    }
+
+
+	
+}
