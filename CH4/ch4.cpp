a362d233880b324f79a4ac08e5ac66db86647e07
Adam Toth <46538598+tothadam710@users.noreply.github.com>
2021-11-05 12:08:03 +0100

    Update ch4.cpp

--- CH4/ch4.cpp
+++ CH4/ch4.cpp
@@ -1,61 +1,31 @@
 #include "../std_lib_facilities.h"
 
-//We had to change it thats why I commented it.
 
-/*void expr(double elso,double masodik){
-	if(elso>masodik){
-		cout << "The smaller value is: " << masodik << "\n";
-		cout << "The larger value is: " << elso << "\n";
-		
-		if((elso - masodik < 0.01)){
-			cout << "The numbers almost equal\n";
-		}
-	}
-
-
-	else if(elso<masodik){
-		cout << "The smaller value is: " << elso << "\n";
-		cout << "The larger value is: " << masodik << "\n";
-		
-		if((masodik - elso < 0.01)){
-			cout << "The numbers almost equal\n";
-		}
-	}
-
-	else{
-		cout << "The numbers are equal\n";		
-	}
-}*/
-
-
-
-
-
-double metric(double number,string u){
+double metric(double number, string u) {
 
 	constexpr double cm = 0.01;
 	constexpr double in = 0.0254;
 	constexpr double ft = 0.3048;
 
 
-	if(u=="cm"){
+	if (u == "cm") {
 		return number * cm;
 	}
 
-	else if(u=="in"){
+	else if (u == "in") {
 		return number * in;
 	}
 
 
-	else if(u=="ft"){
+	else if (u == "ft") {
 		return number * ft;
 	}
 
-	else if(u=="m"){
+	else if (u == "m") {
 		return number;
 	}
 
-	else{
+	else {
 		return 0;
 	}
 
@@ -64,10 +34,10 @@ double metric(double number,string u){
 }
 
 
-void kiiras(double s,int d,double lg,double sm){
+void print_values(double s, int d, double lg, double sm) {
 
 
-	cout << "The sum converted to meter: " << s << "\n"; 
+	cout << "The sum converted to meter: " << s << "\n";
 	cout << "The amount of the items: " << d << "\n";
 	cout << "The largest number: " << lg << "\n";
 	cout << "The smallest number: " << sm << "\n";
@@ -75,18 +45,37 @@ void kiiras(double s,int d,double lg,double sm){
 }
 
 
-void sorting(vector<double> v){
+void original_and_sorting(vector<double> v) {
 
-	sort(v.begin(),v.end());
+	cout << "The  original elements of the vector: [";
 
-		cout << "The elements of the vector: [";
+	for (int i = 0; i < v.size(); ++i)
+	{			
+			cout << v[i] << ";";
 
-		for (int i = 0; i < v.size(); ++i)
-		{
-			cout  << v[i] << ";";
+			if (i == v.size() -1 ) {
+				cout << v[i];
+			}
+	}
+
+	cout << "]";
+
+	cout << "\n";
+
+	sort(v.begin(), v.end());
+
+	cout << "The  sorted elements of the vector: [";
+
+	for (int i = 0; i < v.size(); ++i)
+	{
+		cout << v[i] << ";";
+
+		if (i == v.size() - 1) {
+			cout << v[i];
 		}
+	}
 
-		cout << "]";
+	cout << "]";
 
 
 }
@@ -95,75 +84,75 @@ void sorting(vector<double> v){
 int main()
 
 {
-	double num1,num2,sum;
-	double largest = 0,smallest = 0;
-	
+	double num1, num2, sum = 0;
+	double largest = 0, smallest = 0;
+
 	string unit;
 	int db = 0;
 
 	vector<double> vector_1;
 
 
-	
-	
+
+
 	cout << "Please type a number and if you want ot terminate type '|' to the end of it:\n";
 
 
-	while(cin >> num1 >> unit ){
+	while (cin >> num1 >> unit) {
 
-		
 
-		if(unit == "cm" || unit == "m" || unit == "ft" || unit == "in"){
 
-		db++;
+		if (unit == "cm" || unit == "m" || unit == "ft" || unit == "in") {
+
+			db++;
 
 		}
 
 		//cout << "Please type 2 numbers and if you want ot terminate type '|' to the end of it:\n";
-	
+
 		//expr(num1,num2);
 
 
 		if (smallest == 0 && largest == 0) {
-                smallest = num1;
-                largest = num1;
-            }
+			smallest = num1;
+			largest = num1;
+		}
 
 
 
-		else if(num1>largest){
-			
+		else if (num1 > largest) {
+
 			largest = num1;
 
 			cout << num1 << " is the largest so far\n";
-			
+
 		}
 
-		
-		else if(num1 < smallest){
-				smallest = num1;
-				cout << num1  << " is the smallest so far\n";
-				
-			}
 
-		
-		
+		else if (num1 < smallest) {
+			smallest = num1;
+			cout << num1 << " is the smallest so far\n";
 
-		sum = sum + metric(num1,unit);
+		}
 
-	
-		vector_1.push_back(metric(num1,unit));
-		
+
+
+
+		sum = sum + metric(num1, unit);
+
+
+		vector_1.push_back(metric(num1, unit));
+
 
 	}
 
 
-	
-		kiiras(sum,db,largest,smallest);
 
-		sorting(vector_1);
+	print_values(sum, db, largest, smallest);
 
-		
+	original_and_sorting(vector_1);
+
+	system("pause");
 
 	return 0;
 
